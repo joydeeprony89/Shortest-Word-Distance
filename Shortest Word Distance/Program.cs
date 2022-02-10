e13af0602f68bb7b3dc0a33ce5e63cb0aa0b232f
jd_786 <jaydeep_shil@certree.com>
2022-02-10 19:22:08 +0530

    Add project files.

---
+++ Shortest Word Distance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Shortest_Word_Distance
+{
+  class Program
+  {
+    // https://aaronice.gitbook.io/lintcode/hash-table/shortest-word-distance
+    // https://www.youtube.com/watch?v=CAIHE6PTpBo
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+      string[] words = new string[] { "practice", "makes", "perfect", "coding", "makes" };
+      string word1 = "coding";
+      string word2 = "practice";
+      int distance = p.ShortestWordDistance(words, word1, word2);
+      Console.WriteLine(distance);
+    }
+
+
+    int ShortestWordDistance(string[] words, string word1, string word2)
+    {
+      int distance = int.MaxValue;
+      int i = -1, j = -1;
+      for (int k = 0; k < words.Length; k++)
+      {
+        if (words[k].Equals(word1))
+          i = k;
+        if (words[k].Equals(word2))
+          j = k;
+
+        if (i != -1 && j != -1)
+        {
+          distance = Math.Min(Math.Abs(i - j), distance);
+        }
+      }
+
+      return distance;
+    }
+  }
+}
